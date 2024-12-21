a9852da8e98e1e3265d4221a3d0fac3a76d554c3
Rakhshan Ali <alirakhshan7070@gmail.com>
2024-12-21 18:19:23 +0530

    miami: clone repos from Motorola-Miami
    https://github.com/Motorola-Miami

--- miamiA15.sh
+++ miamiA15.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-git clone https://github.com/Rakhshan7070/android_device_motorola_miami.git -b 15.0 device/motorola/miami
+git clone https://github.com/Motorola-Miami/android_device_motorola_miami.git -b 15.0 device/motorola/miami
 
-git clone https://github.com/Rakhshan7070/android_device_motorola_sm6375-common.git -b 15.0 device/motorola/sm6375-common
+git clone https://github.com/Motorola-Miami/android_device_motorola_sm6375-common.git -b 15.0 device/motorola/sm6375-common
 
-git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_miami.git -b 15.0 vendor/motorola/miami
+git clone https://github.com/Motorola-Miami/proprietary_vendor_motorola_miami.git -b 15.0 vendor/motorola/miami
 
-git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_sm6375-common.git -b 15.0 vendor/motorola/sm6375-common
+git clone https://github.com/Motorola-Miami/proprietary_vendor_motorola_sm6375-common.git -b 15.0 vendor/motorola/sm6375-common
 
-git clone https://github.com/Rakhshan7070/android_kernel_motorola_sm6375.git -b 15.0 kernel/motorola/miami
+git clone https://github.com/Motorola-Miami/android_kernel_motorola_sm6375.git -b 15.0 kernel/motorola/miami
 
-git clone https://github.com/Rakhshan7070/android_hardware_motorola.git -b 15.0 hardware/motorola
+git clone https://github.com/Motorola-Miami/android_hardware_motorola.git -b 15.0 hardware/motorola
 
 rm -rf hardware/qcom-caf/sm8350/audio
 
-git clone https://github.com/Rakhshan7070/android_hardware_qcom_audio.git -b 15.0-caf-sm8350 hardware/qcom-caf/sm8350/audio
+git clone https://github.com/Motorola-Miami/android_hardware_qcom_audio.git -b 15.0-caf-sm8350 hardware/qcom-caf/sm8350/audio
 

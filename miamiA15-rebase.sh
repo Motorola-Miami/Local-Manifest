3ae9c1ade632a109f8c0bef498589971894f6ae4
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-01-30 00:13:41 +0530

    miami: rebase: Clone Vendor tree from gitlab
    * LFS issues

--- miamiA15-rebase.sh
+++ miamiA15-rebase.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Motorola-Miami/android_device_motorola_miami.git -b
 
 git clone https://github.com/Motorola-Miami/android_device_motorola_sm6375-common.git -b rebase/15.0 device/motorola/sm6375-common
 
-git clone https://github.com/Motorola-Miami/proprietary_vendor_motorola_miami.git -b rebase/15.0 vendor/motorola/miami
+git clone https://gitlab.com/motorola-miami/proprietary_vendor_motorola_miami.git -b rebase/15.0 vendor/motorola/miami
 
 git clone https://github.com/Motorola-Miami/proprietary_vendor_motorola_sm6375-common.git -b rebase/15.0 vendor/motorola/sm6375-common
 
